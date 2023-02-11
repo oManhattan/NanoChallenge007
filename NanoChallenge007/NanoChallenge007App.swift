083cc5331268f08cb781e140ccace22436129ad0
oManhattan <matheusbatuque@gmail.com>
2023-02-11 13:52:55 -0300

    Initial Commit

---
+++ NanoChallenge007/NanoChallenge007App.swift
@@ -0,0 +1,17 @@
+//
+//  NanoChallenge007App.swift
+//  NanoChallenge007
+//
+//  Created by Matheus Cavalcanti de Arruda on 11/02/23.
+//
+
+import SwiftUI
+
+@main
+struct NanoChallenge007App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
